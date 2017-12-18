aa69c6d7025472d60ff1dc562f3f29cb1be5a656
XilongPei <pei_xilong@tongji.edu.cn>
2017-12-18 09:41:31 +0800

    fix some typos

--- Openparts-framework/src/main/java/com/cnpc/framework/filter/SystemFilter.java
+++ Openparts-framework/src/main/java/com/cnpc/framework/filter/SystemFilter.java
@@ -1,22 +1,17 @@
 package com.cnpc.framework.filter;
 
 import com.cnpc.framework.utils.SpringContextUtil;
-import com.cnpc.framework.utils.StrUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.Ini;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.util.AntPathMatcher;
 import org.apache.shiro.util.PatternMatcher;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.apache.log4j.Logger;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 
 /**
  * filter过滤器，获取项目路径，设置ajax超时标识
@@ -25,36 +20,36 @@ import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
  */
 public class SystemFilter implements Filter {
 
-
     private static List<String> anonlist;
-
-    private static PatternMatcher pathMatcher=new AntPathMatcher();
-
+    private static PatternMatcher pathMatcher = new AntPathMatcher();
+    static final Logger log = Logger.getLogger(SystemFilter.class);
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException,
             ServletException {
 
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
 
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        System.out.println(request.getRequestURL());
+        log.debug(request.getRequestURL());
+
         String basePath = request.getContextPath();
+        boolean isauth = SecurityUtils.getSubject().isAuthenticated();
+
         request.setAttribute("basePath", basePath);
-        request.setAttribute("isauth",SecurityUtils.getSubject().isAuthenticated());
-        if (!SecurityUtils.getSubject().isAuthenticated()) {
+        request.setAttribute("isauth", isauth);
+        if (! isauth) {
             //判断session里是否有用户信息
             if (request.getHeader("x-requested-with") != null
                     && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
                 //如果是ajax请求响应头会有，x-requested-with
                 //如果是anno的请求，则直接通过
                 if (!matchAnonPath(request.getRequestURI(),basePath)) {
-                    response.setHeader("session-status", "timeout");//在响应头设置session状态
+                    response.setHeader("session-status", "timeout");    //在响应头设置session状态
                     return;
                 }
             }
         }
         filterChain.doFilter(request, servletResponse);
-
     }
 
     @Override
@@ -70,7 +65,6 @@ public class SystemFilter implements Filter {
         // TODO Auto-generated method stub
 
     }
-
 
     public List<String> getAnonlist() {
         if (anonlist != null) {
