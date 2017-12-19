cf04304149140e6b7f2a4a8aadc48f9c77b8432f
XilongPei <pei_xilong@tongji.edu.cn>
2017-12-19 14:35:22 +0800

    remove filter: AccessTokenFilter, add its function into SystemFilter

--- Openparts-framework/src/main/java/com/cnpc/framework/filter/SystemFilter.java
+++ Openparts-framework/src/main/java/com/cnpc/framework/filter/SystemFilter.java
@@ -1,6 +1,9 @@
 package com.cnpc.framework.filter;
 
 import com.cnpc.framework.utils.SpringContextUtil;
+import com.cnpc.framework.utils.AccessToken;
+import com.cnpc.framework.base.dao.RedisDao;
+import com.cnpc.framework.constant.RedisConstant;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.util.AntPathMatcher;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Resource;
 
 /**
  * filter过滤器，获取项目路径，设置ajax超时标识
@@ -20,81 +24,135 @@ import java.util.List;
  */
 public class SystemFilter implements Filter {
 
-    private static List<String> anonlist;
-    private static PatternMatcher pathMatcher = new AntPathMatcher();
-    static final Logger log = Logger.getLogger(SystemFilter.class);
+	private RedisDao redisDao = null;
 
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException,
-            ServletException {
+	private static List<String> anonlist;
+	private static List<String> access_token_list;
+	private static PatternMatcher pathMatcher = new AntPathMatcher();
+	static final Logger log = Logger.getLogger(SystemFilter.class);
 
-        HttpServletRequest request = (HttpServletRequest)servletRequest;
-        HttpServletResponse response = (HttpServletResponse)servletResponse;
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
 
-        log.debug(request.getRequestURL());
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        String basePath = request.getContextPath();
-        boolean isauth = SecurityUtils.getSubject().isAuthenticated();
+		log.debug(request.getRequestURL());
 
-        request.setAttribute("basePath", basePath);
-        request.setAttribute("isauth", isauth);
-        if (! isauth) {
-            //判断session里是否有用户信息
-            if (request.getHeader("x-requested-with") != null
-                    && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
-                //如果是ajax请求响应头会有，x-requested-with
-                //如果是anno的请求，则直接通过
-                if (!matchAnonPath(request.getRequestURI(),basePath)) {
-                    response.setHeader("session-status", "timeout");    //在响应头设置session状态
-                    return;
+		String basePath = request.getContextPath();
+		boolean isauth = SecurityUtils.getSubject().isAuthenticated();
+
+		request.setAttribute("basePath", basePath);
+		request.setAttribute("isauth", isauth);
+		if (!isauth) {
+
+           if (matchAccessTokenPath(request.getRequestURI(), basePath)) {
+    			String access_token = request.getParameter("access_token");
+    			String key = RedisConstant.ACCESS_TOKEN_PRE + access_token;
+
+                if (redisDao == null) {
+                    redisDao = (RedisDao)SpringContextUtil.getBean("redisDao");
                 }
+    			String accessStr = redisDao.get(key);
+
+    			if (!AccessToken.checkExpiresIn(accessStr)) {
+                    // 在响应头设置session状态
+    				response.setHeader("session-status", "timeout");
+    				return;
+    			}
             }
-        }
-        filterChain.doFilter(request, servletResponse);
-    }
+            else {
+                String str = request.getHeader("x-requested-with");
+                if ( str != null && str.equalsIgnoreCase("XMLHttpRequest")) {
+    				// 如果是ajax请求响应头会有，x-requested-with
+    				// 如果是anno的请求，则直接通过
+    				if (!matchAnonPath(request.getRequestURI(), basePath)) {
+                        // 在响应头设置session状态
+    					response.setHeader("session-status", "timeout");
+    					return;
+    				}
+			    }
 
-    @Override
-    public void destroy() {
-
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void init(FilterConfig arg0) throws ServletException {
-
-        // TODO Auto-generated method stub
-
-    }
-
-    public List<String> getAnonlist() {
-        if (anonlist != null) {
-            return anonlist;
-        } else {
-            Ini.Section section = (Ini.Section) SpringContextUtil.getBean("filterChainDefinitionClass");
-            anonlist = new ArrayList<>();
-            for (String key : section.keySet()) {
-                if (section.get(key).equals("anon")) {
-                    anonlist.add(key);
-                }
+                // FILTER setting: spring-shiro.xml
+                // /**=authc
+                //
+                // authc：该过滤器下的页面必须验证后才能访问,它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter
             }
-            return anonlist;
-        }
-    }
+		}
+		filterChain.doFilter(request, servletResponse);
+	}
 
-    public boolean matchAnonPath(String path,String basePath) {
-        path=path.replace(basePath,"");
-        List<String> alist = this.getAnonlist();
-        boolean match = false;
-        for (String s : alist) {
-            if (pathMatches(s,path)) {
-                match = true;
-                break;
-            }
-        }
-        return match;
-    }
+	@Override
+	public void destroy() {
 
-    protected boolean pathMatches(String pattern, String path) {
-        return pathMatcher.matches(pattern, path);
-    }
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<String> getAnonlist() {
+		if (anonlist != null) {
+			return anonlist;
+		} else {
+			Ini.Section section = (Ini.Section) SpringContextUtil.getBean("filterChainDefinitionClass");
+			anonlist = new ArrayList<>();
+			for (String key : section.keySet()) {
+				if (section.get(key).equals("anon")) {
+					anonlist.add(key);
+				}
+			}
+			return anonlist;
+		}
+	}
+
+	public boolean matchAnonPath(String path, String basePath) {
+		path = path.replace(basePath, "");
+		List<String> alist = this.getAnonlist();
+		boolean match = false;
+		for (String s : alist) {
+			if (pathMatches(s, path)) {
+				match = true;
+				break;
+			}
+		}
+		return match;
+	}
+
+	public List<String> getAccessTokenList() {
+		if (access_token_list != null) {
+			return access_token_list;
+		} else {
+			Ini.Section section = (Ini.Section) SpringContextUtil.getBean("filterChainDefinitionClass");
+			access_token_list = new ArrayList<>();
+			for (String key : section.keySet()) {
+				if (section.get(key).equals("access_token")) {
+					access_token_list.add(key);
+				}
+			}
+			return access_token_list;
+		}
+	}
+
+	public boolean matchAccessTokenPath(String path, String basePath) {
+		path = path.replace(basePath, "");
+		List<String> alist = this.getAccessTokenList();
+		boolean match = false;
+		for (String s : alist) {
+			if (pathMatches(s, path)) {
+				match = true;
+				break;
+			}
+		}
+		return match;
+	}
+
+	protected boolean pathMatches(String pattern, String path) {
+		return pathMatcher.matches(pattern, path);
+	}
 }
