435016afee6eccb8bc9bd5758ddf26a9a69159fc
XilongPei <pei_xilong@tongji.edu.cn>
2018-01-05 10:45:24 +0800

    fix some typos

--- Openparts-web/src/main/java/com/openparts/base/controller/API_Controller.java
+++ Openparts-web/src/main/java/com/openparts/base/controller/API_Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import com.openparts.common.OP_Errors;
-
+import com.openparts.common.CommonConstants;
 import com.openparts.utils.mongodb.GridFSClient;
 
 @Controller
@@ -17,6 +17,7 @@ public class API_Controller {
     private static final Logger LOGGER = LoggerFactory.getLogger(API_Controller.class);
 
     @RequestMapping(value = "/api/{res}", method = RequestMethod.POST)
+    @ResponseBody
     public String do_API(@RequestParam(value = "access_token", required = false, defaultValue = "") String code, @PathVariable(value = "res") String res,
                            HttpServletRequest request, Model model) {
         if (res == null)
